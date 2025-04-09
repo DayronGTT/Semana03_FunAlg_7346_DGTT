8eb6b7b198f0bd3f294574ba6215a27bc40c8352
DayGio <KITTDRAXT@upn.pe>
2025-04-08 19:16:50 -0500

    Pude crear otro proyecto en C#

---
+++ Ejercicio2(haré mas detalado en casa)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_haré_mas_detalado_en_casa_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

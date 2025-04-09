4a6d4f84d8f84d26bd2a3f4c0004e4c5e556a2f9
DayGio <KITTDRAXT@upn.pe>
2025-04-08 19:07:19 -0500

    primer algoritmo de suma en C#

--- C#Teórico_Ejercicio1/Program.cs
+++ C#Teórico_Ejercicio1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace C_Teórico_Ejercicio1
     {
         static void Main(string[] args)
         {
+            int n1, n2, Su;
+            //Console.ReadKey();
+            Console.Write("Ingrese el primer número: ");
+            n1 = int.Parse(Console.ReadLine());
+            
+            Console.Write("Ingrese el segundo número: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            Su = n1 + n2;
+            Console.Write("La respuesta es "+Su);
         }
     }
 }
